ab9569a7684796d01b146c1218ceb539d34d3f5f
Dodo20987 <jaydenthung@hotmail.com>
2024-09-23 19:07:09 -0700

    The script may now register for events when provided a link

---
+++ src/testing.py
@@ -0,0 +1,18 @@
+import main
+
+def test_press_register():
+    for i in range(0, 50):
+        e1 = main.registerEvent()
+        if e1.clickRegister():
+            e1.completeRegister()
+
+def test_info_input():
+    for i in range(0, 25):
+        e1 = main.registerEvent()
+        e1.click_register()
+        if e1.login_page():
+            e1.complete_register()
+
+if __name__ == "__main__":
+    #test_press_register()
+    test_info_input()
